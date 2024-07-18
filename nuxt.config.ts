ef3fd70a1e760a75d16b349c01b358465c7cd9f4
felixfavour <felixfavour0@gmail.com>
2024-07-18 09:23:27 +0100

    chore: initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  compatibilityDate: '2024-07-18',
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/seo"],
+  site: {
+    url: "https://cloudofworship.com",
+    name: "Cloud of Worship",
+    description: "A free browser-based presentation software to craft the best on-screen experience for your church.",
+    defaultLocale: "en",
+  },
+  css: ["@/assets/main.css"],
 })
