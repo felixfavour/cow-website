21c4e94be24efc221bb382516da001cc2e4791ac
felixfavour <felixfavour0@gmail.com>
2024-09-21 18:22:35 +0100

    chore: updated changelog

--- pages/changelog.vue
+++ pages/changelog.vue
@@ -83,6 +83,7 @@ const changelogs = [
       "Added public domain Bible versions and other improvements. (log out to enable)",
       "Added more quick actions for easier navigation.",
       "Improved access to scriptures for better performance.",
+      "Convert hymn slides to song slides for song slide benefits.",
       "Implemented clear labeling and structure for app-wide actions.",
       "The schedule modal now opens on load, with various fixes.",
     ],
