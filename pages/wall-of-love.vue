40d3ca54a04d090d9ec5c2e8c490a482b9a64364
felixfavour <felixfavour0@gmail.com>
2025-12-18 05:55:38 +0100

    chore: updated numbers

--- pages/wall-of-love.vue
+++ pages/wall-of-love.vue
@@ -23,7 +23,7 @@
           Get started for free
         </a>
         <div class="helper-text text-sm text-center pt-2">
-          serving <span class="font-semibold">1000+</span> churches
+          serving <span class="font-semibold">1,200+</span> churches
         </div>
       </div>
     </div>
